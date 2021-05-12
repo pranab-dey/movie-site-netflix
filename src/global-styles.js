8529b9d2edd4c47dca9610d0a637d196a8534952
pranab-dey <pranab.strings07@gmail.com>
2021-05-12 22:41:57 +0600

    accordion implemented"

--- src/global-styles.js
+++ src/global-styles.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 
 @import url('https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
 html,body {
     font-family: 'Newsreader', serif;
     -webkit-font-smoothing : antialiased;
@@ -10,6 +11,12 @@ html,body {
     background-color: black;
     color: white;
     font-size: 16px;
+    
+}
+
+a {
+    cursor: pointer;
+    text-decoration: none;
 }
 
 `;
